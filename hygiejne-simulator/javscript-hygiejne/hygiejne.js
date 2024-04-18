4b129235cfe3535b190756aee1a129fa5280866b
omar-eaa <143701116+omar-eaa@users.noreply.github.com>
2024-04-18 11:55:17 +0200

    hygie

---
+++ hygiejne-simulator/javscript-hygiejne/hygiejne.js
@@ -0,0 +1,5 @@
+const divBoks = document.querySelector(".boks");
+const rightBtn = document.querySelector(".hygienje.videre.knap");
+rightBtn.addEventListener("click", () => {
+  divBoks.style.display = "block";
+});
