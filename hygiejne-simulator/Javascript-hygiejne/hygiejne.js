f5bc5544234e5341f7585a1e4b4e3e27ceacc0c5
eaa23mgja <eaa23mgja@students.eaaa.dk>
2024-04-18 13:20:57 +0200

    long waited commit comming now

--- hygiejne-simulator/Javascript-hygiejne/hygiejne.js
+++ hygiejne-simulator/Javascript-hygiejne/hygiejne.js
@@ -3,3 +3,10 @@ const rightBtn = document.querySelector(".hygiejne-videre-knap");
 rightBtn.addEventListener("click", () => {
 divBoks.style.display = 'block';
 })
+
+document.addEventListener("DOMContentLoaded", function (){
+    const vide = document.getElementById("vide"); 
+    vide.addEventListener("click", function(){
+        window.location.href = "."
+    })
+})
