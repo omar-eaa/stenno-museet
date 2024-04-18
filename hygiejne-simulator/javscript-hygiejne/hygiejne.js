c67d1685407b901f0d3ba1c65c9bec60d826cb31
omar-eaa <143701116+omar-eaa@users.noreply.github.com>
2024-04-18 14:44:26 +0200

    alot but almost done

--- hygiejne-simulator/javscript-hygiejne/hygiejne.js
+++ hygiejne-simulator/javscript-hygiejne/hygiejne.js
@@ -3,3 +3,28 @@ const rightBtn = document.querySelector(".hygiejne-videre-knap");
 rightBtn.addEventListener("click", () => {
   divBoks.style.display = "block";
 });
+
+// knap tilabge til side 3
+document.addEventListener("DOMContentLoaded", function () {
+  const tilbage = document.getElementById("tilbage");
+  tilbage.addEventListener("click", function () {
+    window.location.href = "../hygiejne-simulator/hygiejne-side3.html";
+  });
+});
+
+// rigtig
+document.addEventListener("DOMContentLoaded", function () {
+  const rigtig = document.getElementById("rigtig");
+  rigtig.addEventListener("click", function () {
+    window.location.href = "../hygiejne-simulator/hygiejne-rigtig2.html";
+  });
+});
+
+// forkert
+
+document.addEventListener("DOMContentLoaded", function () {
+  const forkert = document.getElementById("forkert");
+  forkert.addEventListener("click", function () {
+    window.location.href = "../hygiejne-simulator/hygiejne-forkert2.html";
+  });
+});
