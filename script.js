b5f97cb48723ebca00f6eae372850dbdd9a3a1ae
eaa23mgja <eaa23mgja@students.eaaa.dk>
2024-04-18 14:28:17 +0200

    forside

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 // vacciner
 // simulator
 
-document.addEventListener("DOMContentLoaded", function () {
-  const epidemier = document.getElementById("epidemier");
-  epidemier.addEventListener("click", function () {
-    window.location.href = "./tidslinje/html-sider/tidslinje.html";
-  });
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   const epidemier = document.getElementById("epidemier");
+//   epidemier.addEventListener("click", function () {
+//     window.location.href = "./tidslinje/html-sider/tidslinje.html";
+//   });
+// });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const vacciner = document.getElementById("vacciner");
-  vacciner.addEventListener("click", function () {
-    window.location.href = "./vaccine/vaccine-forside.html";
-  });
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   const vacciner = document.getElementById("vacciner");
+//   vacciner.addEventListener("click", function () {
+//     window.location.href = "./vaccine/vaccine-forside.html";
+//   });
+// });
