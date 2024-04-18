0b6926848aabd98c6d5507695443a801deaaf3d3
omar-eaa <143701116+omar-eaa@users.noreply.github.com>
2024-04-18 13:16:37 +0200

    updated navigation for hygiejne-simulator

--- hygiejne-simulator/Javascript-hygiejne/hygiejne.js
+++
@@ -1,5 +0,0 @@
-const divBoks = document.querySelector(".boks");
-const rightBtn = document.querySelector(".hygiejne-videre-knap");
-rightBtn.addEventListener("click", () => {
-divBoks.style.display = 'block';
-})
